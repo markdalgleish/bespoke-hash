3d225c8a68f058584f1e9171c1a4a5f586c6ce88
Dan Allen <dan.j.allen@gmail.com>
2017-10-03 02:57:00 -0600

    resolves #6 init synchronously unless deferStart option is true

--- dist/bespoke-hash.js
+++ dist/bespoke-hash.js
@@ -7,7 +7,7 @@
  */
 
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g=(g.bespoke||(g.bespoke = {}));g=(g.plugins||(g.plugins = {}));g.hash = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = function() {
+module.exports = function(opts) {
   return function(deck) {
     var activateSlide = function(index) {
       var indexToActivate = -1 < index && index < deck.slides.length ? index : 0;
@@ -33,7 +33,7 @@ module.exports = function() {
       }
     };
 
-    setTimeout(function() {
+    var init = function() {
       parseHash();
 
       deck.on('activate', function(e) {
@@ -42,7 +42,14 @@ module.exports = function() {
       });
 
       window.addEventListener('hashchange', parseHash);
-    }, 0);
+    }
+
+    // honor option to enable legacy behavior
+    if ((opts || {}).deferStart) {
+      setTimeout(init, 0);
+    } else {
+      init();
+    }
   };
 };
 
